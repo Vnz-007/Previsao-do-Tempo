c43e659827e6ab5a2323ae1bbc229b080394b25a
Vinícius Loiola <viniciusloiolajg@gmail.com>
2025-03-03 15:45:36 -0300

    CommitFinal

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function colocarDadosNaTela(dados) {
   document.querySelector(".texto-previsao").innerHTML =
     dados.weather[0].description;
   document.querySelector(".umidade").innerHTML =
-    "Umidade" + dados.main.humidity + "%";
+    "Umidade: " + dados.main.humidity + "%";
   document.querySelector(
     ".img-previsao"
   ).src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
